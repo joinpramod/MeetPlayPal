dd62ca471a745f0d0f35ac084062bd0ad5c78187
joinpramod <pramodh.suresh@yahoo.com>
2022-03-20 16:37:50 -0400

    bnvnbv

---
+++ MeetPlayPal/Controllers/DashboardController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeetPlayPal.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Interaction()
+        {
+            return View();
+        }
+
+        public ActionResult MessageList()
+        {
+            return View();
+        }
+
+        public ActionResult PartnerList()
+        {
+            return View();
+        }
+
+        public ActionResult PostMessage()
+        {
+            return View();
+        }
+    }
+}
