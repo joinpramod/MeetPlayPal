3c12eeea964be161d1ce921a5c96fb3951c9d7d5
joinpramod <pramodh.suresh@yahoo.com>
2022-06-10 10:39:37 -0400

    new changes

--- MeetPlayPal/Controllers/ActivityController.cs
+++ MeetPlayPal/Controllers/ActivityController.cs
@@ -28,5 +28,9 @@ namespace MeetPlayPal.Controllers
         {
             return View();
         }
+        public ActionResult Search()
+        {
+            return View();
+        }
     }
 }
