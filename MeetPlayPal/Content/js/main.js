cc79f56e3f9f55e5cf1bfd3978c8915e0289ad71
joinpramod <pramodh.suresh@yahoo.com>
2022-07-08 16:58:54 -0400

    new changes

--- MeetPlayPal/Content/js/main.js
+++ MeetPlayPal/Content/js/main.js
@@ -336,11 +336,14 @@ jQuery(document).on('ready', function() {
 		tinymce.init({
 			selector: 'textarea#tg-tinymceeditor',
 			height: 250,
-			theme: 'modern',
-			plugins: [ 'advlist autolink lists link image charmap print preview hr anchor pagebreak', 'searchreplace wordcount visualblocks visualchars code fullscreen', 'insertdatetime media nonbreaking save table contextmenu directionality', 'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc help'],
-			toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-			toolbar2: 'print preview media | forecolor backcolor emoticons | codesample help',
-			image_advtab: true,
+			/*theme: 'modern',*/
+			menubar: '',
+			/*plugins: [ 'advlist autolink lists link image charmap print preview hr anchor pagebreak', 'searchreplace wordcount visualblocks visualchars code fullscreen', 'insertdatetime media nonbreaking save table contextmenu directionality', 'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc help'],*/
+			plugins: 'link',
+			/*toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',*/
+			/*toolbar1: '',*/
+			/*toolbar2: 'print preview media | forecolor backcolor emoticons | codesample help',*/
+			image_advtab: false,
 		});
 	}
 	/*--------------------------------------
--- MeetPlayPal/Controllers/ActivityController.cs
+++ MeetPlayPal/Controllers/ActivityController.cs
@@ -12,7 +12,37 @@ namespace MeetPlayPal.Controllers
         // GET: Dashboard
         public ActionResult Interaction()
         {
-            return View();
+            //List<VwSolutionsModel> solns = new List<VwSolutionsModel>();
+            //solns = GetMyAns(solns);
+            PagingInfo info = new PagingInfo();
+            info.SortField = " ";
+            info.SortDirection = " ";
+            info.PageSize = 20;
+            info.PageCount = 10; //Convert.ToInt32(Math.Ceiling((double)(solns.Count() / info.PageSize)));
+            info.CurrentPageIndex = 0;
+            // var query = solns.OrderBy(c => c.QuestionID).Take(info.PageSize);
+            ViewBag.PagingInfo = info;
+            // return View(query.ToList());
+
+            List<Message> lstMsg = new List<Message>();
+            Message msg = new Message();
+            msg.MessageText = "testing 123 testing 123 testing 123 testing 123 testing 123 testing 123 testing 123 ";
+            msg.ModifiedDateTime = DateTime.Now;
+            msg.Status_Code = "Ping Pong";
+            msg.MsgFrom = 1;
+            msg.MessageId = 234;
+            lstMsg.Add(msg);
+
+            msg = new Message();
+            msg.MessageText = "testing 123 testing 123 testing 123 testing 123 testing 123 testing 123 testing 123 ";
+            msg.ModifiedDateTime = DateTime.Now;
+            msg.Status_Code = "Ping Pong";
+            msg.MsgFrom = 2;
+            msg.MessageId = 4354;
+            lstMsg.Add(msg);
+
+            return View(lstMsg);
+
         }
 
         public ActionResult MessageList()
@@ -54,7 +84,36 @@ namespace MeetPlayPal.Controllers
 
         public ActionResult PartnerList()
         {
-            return View();
+            //List<VwSolutionsModel> solns = new List<VwSolutionsModel>();
+            //solns = GetMyAns(solns);
+            PagingInfo info = new PagingInfo();
+            info.SortField = " ";
+            info.SortDirection = " ";
+            info.PageSize = 20;
+            info.PageCount = 10; //Convert.ToInt32(Math.Ceiling((double)(solns.Count() / info.PageSize)));
+            info.CurrentPageIndex = 0;
+            // var query = solns.OrderBy(c => c.QuestionID).Take(info.PageSize);
+            ViewBag.PagingInfo = info;
+            // return View(query.ToList());
+
+            List<Message> lstMsg = new List<Message>();
+            Message msg = new Message();
+            msg.MessageText = "testing 123 testing 123 testing 123 testing 123 testing 123 testing 123 testing 123 ";
+            msg.ModifiedDateTime = DateTime.Now;
+            msg.Status_Code = "Ping Pong";
+            msg.MsgFrom = 1;
+            msg.MessageId = 234;
+            lstMsg.Add(msg);
+
+            msg = new Message();
+            msg.MessageText = "testing 123 testing 123 testing 123 testing 123 testing 123 testing 123 testing 123 ";
+            msg.ModifiedDateTime = DateTime.Now;
+            msg.Status_Code = "Ping Pong";
+            msg.MsgFrom = 2;
+            msg.MessageId = 4354;
+            lstMsg.Add(msg);
+
+            return View(lstMsg);
         }
 
         public ActionResult PostMessage()
@@ -62,6 +121,11 @@ namespace MeetPlayPal.Controllers
             return View();
         }
         public ActionResult Search()
+        {
+            return View();
+        }
+
+        public ActionResult CurrentPals()
         {
             return View();
         }
